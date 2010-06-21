8c6fe54a33e1f0bec0e894f34cf96983b73f732b
liucougar <liucougar@gmail.com>
2010-06-21 14:51:25 -0700

    added test coverage for skip with failuredetail (-d option)

--- tests/test_details.py
+++ tests/test_details.py
@@ -3,17 +3,21 @@ from tests import SubunitPluginTester
 import sunit
 
 from nose.plugins.failuredetail import FailureDetail
+from nose.plugins.skip import Skip
+from nose import SkipTest
 from testtools.content import Content
 
 class TestDetails(SubunitPluginTester):
     #enable FailureDetail nose plugin
-    plugins = [sunit.Subunit(),FailureDetail()]
-    args = ['-d'] #enable FailureDetail
+    plugins = [sunit.Subunit(),FailureDetail(),Skip()]
+    args = ['-d'] #enable FailureDetail, Skip is enabled by default
     def runTest(self):
         self.getFedSubunitServer()
         result = self.testResult
-        self.assertEqual(result.testsRun, 1)
+        self.assertEqual(result.testsRun, 2)
         self.assertEqual(len(result.failures), 1)
+        self.assertEqual(len(result.errors), 0)
+        self.assertEqual(len(result.skips), 1)
         self.assertTrue("multipart" in self.output)
         self.assertTrue(result.failures[0][1].has_key("traceback"))
         self.assertTrue(issubclass(result.failures[0][1]["traceback"].__class__, Content))
@@ -24,4 +28,7 @@ class TestDetails(SubunitPluginTester):
         class failuredetail(unittest.TestCase):
             def runTest(self):
                 self.assertTrue(False)
-        return unittest.TestSuite([failuredetail()])
+        class skip(unittest.TestCase):
+            def runTest(self):
+                raise SkipTest()
+        return unittest.TestSuite([failuredetail(), skip()])
