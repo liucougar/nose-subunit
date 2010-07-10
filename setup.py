1f95823d09210789379c094696beb1a6a5edd292
liucougar <liucougar@gmail.com>
2010-07-10 14:43:26 -0700

    specify testtools>=0.9.4 in dependency to workaround insufficient dependency requirement in python-subunit 0.0.6 see https://bugs.launchpad.net/subunit/+bug/604011

--- setup.py
+++ setup.py
@@ -13,6 +13,8 @@ setup(
     install_requires = [
         "nose>=0.11.3",
 	"python-subunit",
+	#subunit 0.0.6 should specify this explicitly, but it just specify testtools
+	"testtools>=0.9.4",
     ],
     scripts = [],
     license="BSD",
